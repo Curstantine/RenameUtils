291567309fdcb84b3e927da9f874bba320638311
resistlyfe@gmail.com <resistlyfe@gmail.com>
2023-03-01 20:36:12 +0530

    chore: removed gluon and added tauri

--- backend/index.ts
+++
@@ -1,9 +0,0 @@
-import * as Gluon from "@gluon-framework/gluon";
-
-const isDev = process.env.NODE_ENV === "development";
-const port = process.env.PORT || 5173;
-const target = isDev ? `http://localhost:${port}` : "";
-
-await Gluon.open(target, {
-	allowHTTP: isDev,
-});
